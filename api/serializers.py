4ac81e0076e2d23253a7c3be554cb3e43cd68131
sahla <tcsahla@gmail.com>
2024-09-01 17:16:35 +0530

    second commit

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+class ProductSerializer(serializers.Serializer):
+    name=serializers.CharField()
+    price=serializers.IntegerField()
+    description=serializers.CharField()
+    category=serializers.CharField()
+    # image=serializers.ImageField()
--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.models import Products
+from api.serializers import ProductSerializer
 
 # Create your views here.
+
+class ProductView(APIView):
+    def get(self,request,*args,**kw):
+        qs=Products.objects.all()
+        serializer = ProductSerializer(qs,many=True)
+        return Response(data=serializer.data)
+    
+    def post(self,request,*args,**kw):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            print(serializer.validated_data)
+            Products.objects.create(**serializer.validated_data)
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer._errors)
+    
+class ProductDetailsView(APIView):
+    def get(self,request,*args,**kw):
+        return Response(data='detail of a product')
+    def put(self,request,*args,**kw):
+        return Response(data='Item sucessesfully updated')
+    def delete(self,request,*args,**kw):
+        return Response(data='Item delete')
+
+# seralizers(serialization)
+
